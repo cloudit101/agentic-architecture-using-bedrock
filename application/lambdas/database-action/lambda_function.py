b03951f814f470b6dfea2fe35fd2a46a2e8124e0
Wei Chen <wchemz@amazon.com>
2025-05-01 16:14:21 -0500

    Update lambda to correct sql preventing llm return non-sql result.

--- application/lambdas/database-action/lambda_function.py
+++ application/lambdas/database-action/lambda_function.py
@@ -7,21 +7,17 @@ import uuid
 import time
 from datetime import datetime
 from botocore.exceptions import ConnectTimeoutError, BotoCoreError, ClientError
-
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
             print(f"Encoding datetime: {obj.isoformat()}")
             return obj.isoformat()
         return super().default(obj)
-
 # Configure logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 def lambda_handler(event, context):
     logger.info("Received event: %s", json.dumps(event))
-    
     # Extracting the necessary details from the event
     try:
         agent = event['agent']
@@ -34,52 +30,43 @@ def lambda_handler(event, context):
             "statusCode": 400,
             "body": json.dumps({"error": f"Missing key in event: {e}"})
         }
-    
     # Extract the SQL query from parameters
     sql_query = next((param['value'] for param in parameters if param['name'] == 'sql_query'), None)
     if sql_query:
         sql_query = sql_query.rstrip(';')  # Remove trailing semicolon if present
     logger.info("Initial SQL query: %s", sql_query)
-    
     if not sql_query:
         logger.error("No SQL query found in parameters")
         return {
             "statusCode": 400,
             "body": json.dumps({"error": "No SQL query found in parameters"})
         }
-    
     user_question = next((param['value'] for param in parameters if param['name'] == 'user_question'), None)
     if user_question:
         logger.info("User question: %s", user_question)
-        
     if not user_question:
         logger.error("User question not found in parameters")
         return {
             "statusCode": 400,
             "body": json.dumps({"error": "No User question found in parameters"})
         }
-    
     # Fetch PostgreSQL connection details from environment variables
     rds_host = os.environ.get('DB_HOST', 'petstore-instance-1.c7icaemqq3d4.us-east-1.rds.amazonaws.com')
     rds_db = os.environ.get('DB_NAME', 'petstore')
     rds_port = os.environ.get('DB_PORT', '5432')
     rds_username = os.environ.get('DB_USER', 'postgres')
     rds_password = os.environ.get('DB_PASSWORD', 'petstoremaster')
-
     # Fetch Query Correction AgentID from environment variables
     query_correction_agent_id = os.environ.get('QUERY_CORRECTION_AGENT_ID', '')
-    
     # Initialize Bedrock Agent Runtime client
     # bedrock_agent_runtime_client = boto3.client('bedrock-agent-runtime')
     bedrock_agent_runtime_client=boto3.client(
             service_name="bedrock-agent-runtime", region_name='us-west-2'
         )
     logger.info("bedrock_agent_runtime_client: %s", bedrock_agent_runtime_client)
-    
     # Define maximum number of retries to prevent infinite loops
     MAX_RETRIES = 10
     retries = 0
-    
     while retries < MAX_RETRIES:
         try:
             # Establish the connection to the PostgreSQL RDS database
@@ -91,15 +78,12 @@ def lambda_handler(event, context):
                 password=rds_password
             )
             cursor = connection.cursor()
-            
             logger.info("Executing SQL query: %s", sql_query)
             # Execute the SQL query
             cursor.execute(sql_query)
-            
             # Commit the transaction if it's an INSERT, UPDATE, or DELETE statement
             if sql_query.strip().upper().startswith(("INSERT", "UPDATE", "DELETE")):
                 connection.commit()
-            
             # Fetch results if it's a SELECT statement
             if sql_query.strip().upper().startswith("SELECT"):
                 columns = [desc[0] for desc in cursor.description]
@@ -107,25 +91,19 @@ def lambda_handler(event, context):
                 result = [dict(zip(columns, row)) for row in rows]
             else:
                 result = {"message": "Query executed successfully"}
-            
             logger.info("SQL result: %s", result)
-            
             # Close the cursor and connection
             cursor.close()
             connection.close()
-            
             response_body = {
                 'TEXT': {
                     'body': json.dumps(result, cls=DateTimeEncoder)
                 }
             }
-            
             # If execution is successful, break out of the loop
             break
-        
         except Exception as e:
             logger.error("Error executing query: %s", e, exc_info=True)
-            
             # Create the inputText JSON object
             correctionInputText = {
                 "SQL Statement": sql_query,
@@ -134,17 +112,14 @@ def lambda_handler(event, context):
             }
             # formattedInputString = ', '.join([f"{key}={value}" for key, value in correctionInputText.items()])
             logger.info("correctionInputText: %s", correctionInputText)
-            
             try:
                 # Wait for 2 seconds
                 time.sleep(2)
                 session_id = uuid.uuid4().hex
                 logger.info("Generated session_id: %s", session_id)
-                
                 # Define additional parameters for the agent invocation
                 enable_trace = False  # Set as needed
                 end_session = False    # Set as needed
-
                 # add query_correction_agent_id in the logger:
                 logger.info("Invoke the Bedrock Query correctoin Agent to get a corrected SQL query, agent id is : %s", query_correction_agent_id)
                 # Invoke the Bedrock Agent to get a corrected SQL query
@@ -155,17 +130,13 @@ def lambda_handler(event, context):
                     agentId=query_correction_agent_id,
                     agentAliasId='TSTALIASID'
                 )
-                
                 logger.info("Agent response: %s", agent_response)
-                
                 completion = ""
                 for event in agent_response.get("completion"):
                     chunk = event["chunk"]
                     completion = completion + chunk["bytes"].decode()
-                
-                logger.info("Agent completion: %s", completion)
-                corrected_sql = completion
-                
+                logger.info("Agent completion: %s", completion.split(":")[1].strip())
+                corrected_sql = completion.split(":")[1].strip()
                 if not corrected_sql:
                     logger.error("Agent did not return a corrected SQL query.")
                     response_body = {
@@ -174,10 +145,10 @@ def lambda_handler(event, context):
                         }
                     }
                     break  # Exit the loop since we cannot proceed without a valid query
-                
-                logger.info("Agent provided corrected SQL query: %s", corrected_sql)
-                sql_query = corrected_sql.rstrip(';')  # Update sql_query with corrected version
-            
+                logger.info("Agent provided corrected_SQL query: %s", corrected_sql)
+                #sql_query = corrected_sql.rstrip(';')  # Update sql_query with corrected version
+                sql_query = corrected_sql.split(';')[0]
+                logger.info("Agent provided corrected final SQL query: %s", sql_query)
             except Exception as agent_error:
                 logger.error("Error invoking Bedrock Agent: %s", agent_error, exc_info=True)
                 response_body = {
@@ -186,10 +157,8 @@ def lambda_handler(event, context):
                     }
                 }
                 break  # Exit the loop since agent invocation failed
-            
             retries += 1
             logger.info("Retrying SQL execution (%d/%d)...", retries, MAX_RETRIES)
-    
     else:
         # If maximum retries reached without success
         logger.error("Maximum retries reached. Unable to execute SQL query successfully.")
@@ -198,7 +167,6 @@ def lambda_handler(event, context):
                 'body': {"error": "Maximum retries reached. Unable to execute SQL query successfully."}
             }
         }
-    
     # Construct the function response
     function_response = {
         'actionGroup': actionGroup,
@@ -207,18 +175,14 @@ def lambda_handler(event, context):
             'responseBody': response_body
         }
     }
-    
     session_attributes = event.get('sessionAttributes', {})
     prompt_session_attributes = event.get('promptSessionAttributes', {})
-    
     # Construct the action response
     action_response = {
-        'messageVersion': '1.0', 
+        'messageVersion': '1.0',
         'response': function_response,
         'sessionAttributes': session_attributes,
         'promptSessionAttributes': prompt_session_attributes
     }
-    
     logger.info("Response: %s", action_response)
-    
     return action_response
--- application/lambdas/orchestrator-action/invoke-db-agent-action/lambda_function.py
+++ application/lambdas/orchestrator-action/invoke-db-agent-action/lambda_function.py
@@ -4,20 +4,19 @@ import logging
 import uuid
 import os
 from botocore.exceptions import ClientError
-
 # Set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 # Initialize Bedrock Agent Runtime client
 bedrock_agent_runtime_client = boto3.client('bedrock-agent-runtime')
-
 def lambda_handler(event, context):
     try:
         agent = event['agent']
         actionGroup = event['actionGroup']
         function = event['function']
         parameters = event.get('parameters', [])
+        session_attributes = event.get('sessionAttributes', {})
+        prompt_session_attributes = event.get('promptSessionAttributes', {})
     except KeyError as e:
         logger.error("Missing key in event: %s", e)
         return {
@@ -31,7 +30,6 @@ def lambda_handler(event, context):
         # Generate a unique session ID
         session_id = uuid.uuid4().hex
         logger.info(f"Generated session ID: {session_id}")
-
         # Invoke the Bedrock Agent
         logger.info("Attempting to invoke Bedrock agent...")
         agent_response = bedrock_agent_runtime_client.invoke_agent(
@@ -40,19 +38,12 @@ def lambda_handler(event, context):
             agentId=agent_id,
             agentAliasId=agent_alias_id
         )
-        
         logger.info(f"Agent response: {json.dumps(agent_response, default=str)}")
-        
         completion = ""
         for event in agent_response.get("completion"):
             chunk = event["chunk"]
             completion = completion + chunk["bytes"].decode()
-        
         logger.info(f"Agent ccompletion: {completion}")
-        
-        session_attributes = event.get('sessionAttributes', {})
-        prompt_session_attributes = event.get('promptSessionAttributes', {})
-        
         function_response = {
             'actionGroup': actionGroup,
             'function': function,
@@ -64,15 +55,13 @@ def lambda_handler(event, context):
                 }
             }
         }
-    
         # Process the response
         response_body = {
-            'messageVersion': '1.0', 
+            'messageVersion': '1.0',
             'response': function_response,
             'sessionAttributes': session_attributes,
             'promptSessionAttributes': prompt_session_attributes
         }
-
     except ClientError as e:
         logger.error(f"ClientError when invoking Bedrock agent: {e}")
         function_response = {
@@ -87,12 +76,11 @@ def lambda_handler(event, context):
             }
         }
         response_body = {
-            'messageVersion': '1.0', 
+            'messageVersion': '1.0',
             'response': function_response,
             'sessionAttributes': session_attributes,
             'promptSessionAttributes': prompt_session_attributes
         }
-
     except Exception as e:
         function_response = {
             'actionGroup': actionGroup,
@@ -102,18 +90,15 @@ def lambda_handler(event, context):
                     'TEXT': {
                         'body': 'An unexpected error occurred'
                     }
-                    
                 }
             }
         }
         logger.error(f"Unexpected error: {e}")
         response_body = {
-            'messageVersion': '1.0', 
+            'messageVersion': '1.0',
             'response': function_response,
             'sessionAttributes': session_attributes,
             'promptSessionAttributes': prompt_session_attributes
         }
-    
     logger.info(f"response_body: {response_body}")
-    
     return response_body
